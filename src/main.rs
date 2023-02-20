62ef9efcdc47b1bfaae6cb72a8b7755b216fde6d
avour <avour123@gmail.com>
2023-02-20 14:07:23 +0100

    fix for temp dir

--- src/main.rs
+++ src/main.rs
@@ -62,79 +62,72 @@ async fn start_reels(mut form: Form<ReelsForm<'_>>) -> Json<ReelsConfig> {
 
     println!("FilePath {}", filepath);
 
-    // let duration = utils::get_video_duration(&filepath);
-    // println!("Duration: {}", duration);
+    let duration = utils::get_video_duration(&filepath);
+    println!("Duration: {}", duration);
 
-    // let number_of_reels = utils::get_number_of_reels(duration, form.max_reels_duration);
-    // let extra_seconds = duration % form.max_reels_duration;
-    // println!("Number of reels need: {}", number_of_reels);
+    let number_of_reels = utils::get_number_of_reels(duration, form.max_reels_duration);
+    let extra_seconds = duration % form.max_reels_duration;
+    println!("Number of reels need: {}", number_of_reels);
 
-    // let mut reels: Vec<Reels> = Vec::with_capacity(number_of_reels as usize);
+    let mut reels: Vec<Reels> = Vec::with_capacity(number_of_reels as usize);
 
-    // for index in 0..number_of_reels {
-    //     let output_file = format!(
-    //         "{}reels_{}-{}.{}",
-    //         env::temp_dir().to_str().unwrap(),
-    //         timestamp,
-    //         index,
-    //         form.file.content_type().unwrap().extension().unwrap(),
-    //     );
+    for index in 0..number_of_reels {
+        let output_file = format!(
+            "{}/reels_{}-{}.{}",
+            env::temp_dir().to_str().unwrap(),
+            timestamp,
+            index,
+            form.file.content_type().unwrap().extension().unwrap(),
+        );
 
-    //     let start_time = index * form.max_reels_duration;
+        let start_time = index * form.max_reels_duration;
 
-    //     let end_time = {
-    //         if index + 1 < number_of_reels {
-    //             form.max_reels_duration * (index + 1)
-    //         } else {
-    //             (form.max_reels_duration * (index + 1)) + extra_seconds
-    //         }
-    //     };
+        let end_time = {
+            if index + 1 < number_of_reels {
+                form.max_reels_duration * (index + 1)
+            } else {
+                (form.max_reels_duration * (index + 1)) + extra_seconds
+            }
+        };
 
-    //     let mut command = Command::new("ffmpeg");
-    //     command.args([
-    //         "-i",
-    //         &filepath,
-    //         "-ss",
-    //         &utils::convert_timestamp(start_time),
-    //         "-to",
-    //         &utils::convert_timestamp(end_time),
-    //         "-c",
-    //         "copy",
-    //         &output_file,
-    //     ]);
-    //     println!("{:?}", command.get_args().collect::<Vec<&OsStr>>());
+        let mut command = Command::new("ffmpeg");
+        command.args([
+            "-i",
+            &filepath,
+            "-ss",
+            &utils::convert_timestamp(start_time),
+            "-to",
+            &utils::convert_timestamp(end_time),
+            "-c",
+            "copy",
+            &output_file,
+        ]);
+        println!("{:?}", command.get_args().collect::<Vec<&OsStr>>());
 
-    //     let output = command
-    //         .status()
-    //         .expect("Video processing failed on ffmpeg command");
-    //     assert!(
-    //         output.success(),
-    //         "Video processing failed on ffmpeg command"
-    //     );
-    //     // in bytes
+        let output = command
+            .status()
+            .expect("Video processing failed on ffmpeg command");
+        assert!(
+            output.success(),
+            "Video processing failed on ffmpeg command"
+        );
+        // in bytes
         
-    //     let file_size = std::fs::File::open(&output_file)
-    //         .unwrap()
-    //         .metadata()
-    //         .unwrap()
-    //         .len();
-    //     reels.push(Reels { size: file_size })
-    // }
+        let file_size = std::fs::File::open(&output_file)
+            .unwrap()
+            .metadata()
+            .unwrap()
+            .len();
+        reels.push(Reels { size: file_size })
+    }
 
-    // // delete original file
-    // std::fs::remove_file(&filepath).unwrap();
-
-    // Json(ReelsConfig {
-    //     id: timestamp,
-    //     number_of_reels: number_of_reels,
-    //     reels: reels,
-    //     ext: form.file.content_type().unwrap().extension().unwrap().to_string(),
-    // })
+    // delete original file
+    std::fs::remove_file(&filepath).unwrap();
 
     Json(ReelsConfig {
         id: timestamp,
-        number_of_reels: 0,
-        reels: Vec::new(),
+        number_of_reels: number_of_reels,
+        reels: reels,
         ext: form.file.content_type().unwrap().extension().unwrap().to_string(),
     })
 }
@@ -153,7 +146,7 @@ fn get_reels(query: ReelsQuery) -> ReaderStream![File] {
     ReaderStream! {
 
         for index in query.start_index..query.end_index {
-            let output_file = format!("{}reels_{}-{}.{}", env::temp_dir().to_str().unwrap(), query.id, index, query.ext);
+            let output_file = format!("{}/reels_{}-{}.{}", env::temp_dir().to_str().unwrap(), query.id, index, query.ext);
         
             if let Ok(file) = File::open(&output_file).await {
                 yield file;
